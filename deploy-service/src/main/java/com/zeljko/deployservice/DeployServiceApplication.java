96ed51ffeb205c5a2f001df669a964048e419e0a
zstojkovic00 <00zeljkostojkovic@gmail.com>
2024-02-19 21:08:35 +0100

    Initial commit deploy service

---
+++ deploy-service/src/main/java/com/zeljko/deployservice/DeployServiceApplication.java
@@ -0,0 +1,13 @@
+package com.zeljko.deployservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeployServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeployServiceApplication.class, args);
+	}
+
+}
